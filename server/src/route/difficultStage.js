7f53832905b67f4f010b09fe0e55227c5bfffa57
Joel Leo <leo.qiyi.joel@dhs.sg>
2020-07-05 20:07:58 +0800

    bug fix

--- server/src/route/difficultStage.js
+++ server/src/route/difficultStage.js
@@ -6,13 +6,7 @@ var cors = require('cors')
 //setting up CORS settings
 var whitelist = ['http://localhost:1234'];
 var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
+  origin: "*",
   'preflightContinue':true,
   'credentials': true
 }
