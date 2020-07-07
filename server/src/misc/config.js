f5381885552df5b4f1e5e59df5a763df6793b322
Joel Leo <leo.qiyi.joel@dhs.sg>
2020-07-07 18:25:04 +0800

    test commit

--- server/src/misc/config.js
+++ server/src/misc/config.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  easyKey: ["123", "1234"],
+  easyKey: {"123": "1234", "hello": "world"},
   whitelistURL: ['http://localhost:1234', "https://serenesmartlava--20y5c35liu.repl.co", "https://stem--rea123.repl.co", "https://stemmain--kgwxf.repl.co"]
 };
