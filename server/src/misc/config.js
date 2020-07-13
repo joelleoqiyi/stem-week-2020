551afb25de57a467eebb75cc6755a3f9e2f45e28
Joel Leo <leo.qiyi.joel@dhs.sg>
2020-07-13 23:29:16 +0800

    updated config.js with return url

--- server/src/misc/config.js
+++ server/src/misc/config.js
@@ -6,49 +6,49 @@ module.exports = {
       "answer": "pacman",
       "keyName": "e0",
       "keyGiven": "a2luZ29sZGlzZ29k",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/easy_stage1.html"
     },
     {
       "answer": "minecraft",
       "keyName": "e1",
       "keyGiven": "c2F2ZW15d29ybGRz",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/easy_stage1.html"
     },
     {
       "answer": "nintendo",
       "keyName": "e2",
       "keyGiven": "d2hvYXJleW91",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/easy_stage1.html"
     },
     {
       "answer": "felix",
       "keyName": "e3",
       "keyGiven": "c3RlbXdlZWtiZXN0",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/easy_stage1.html"
     },
     {
       "answer": "eden",
       "keyName": "e4",
       "keyGiven": "d2hvYW1p",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/easy_stage1.html"
     },
     {
       "answer": "jnuw17",
       "keyName": "e5",
       "keyGiven": "ZG95b3VsaWtlY2lwaGVy",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/easy_stage2.html"
     },
     {
       "answer": "eukolis",
       "keyName": "e6",
       "keyGiven": "d2hvZG95b3VsaWtl",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/easy_stage4.html"
     },
     {
       "answer": "147852",
       "keyName": "e7",
       "keyGiven": "Y2xhcGNsYXBjbGFw",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/easy_stage5.html"
     }
   ],
   difficultStageAnswers:[
@@ -57,59 +57,59 @@ module.exports = {
       "keyName": "d0",
       "keyGiven": "SEUxMU9fVzBSTERE",
       "imageGiven": "p1.png",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/hard_stage1.html"
     },
     {
       "answer": "1975",
       "keyName": "d1",
       "keyGiven": "SjAxTl9JQ0NfTjB3",
       "imageGiven": "p2.png",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/hard_stage1.html"
     },
     {
       "answer": "snake",
       "keyName": "d2",
       "keyGiven": "SjAxTl9jMG1QdXQxbmdfTjB3",
       "imageGiven": "p3.png",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/hard_stage1.html"
     },
     {
       "answer": "javascript",
       "keyName": "d3",
       "keyGiven": "YzAwa2llc19hUmVfdjNyWV9zQWYz",
       "imageGiven": "p4.png",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/hard_stage1.html"
     },
     {
       "answer": "iphone5",
       "keyName": "d4",
       "keyGiven": "aDB3X0FCMHVUX2wwY0FsU3RyMFJBZzNf",
       "imageGiven": "p5.jpg",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/hard_stage1.html"
     },
     {
       "answer": "80816",
       "keyName": "d5",
       "keyGiven": "MHJfczM1NWlvbjVUMHJhRzNf",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/hard_stage21.html"
     },
     {
       "answer": "red",
       "keyName": "d6",
       "keyGiven": "Y0FuX3kwdV90SDFOa18wZl9hTnl0aDFuR19lMTVl",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/hard_stage22.html"
     },
     {
       "answer": "8",
       "keyName": "d7",
       "keyGiven": "cDFFYTVFX2QwblRfX2hBY0tfX00z",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/hard_stage23.html"
     },
     {
       "answer": "crashcourse",
       "keyName": "d8",
       "keyGiven": "eUF5X0NlbDNCckF0MW9u",
-      "url": "https://google.com"
+      "url": "https://stem-week-cipher-web.herokuapp.com/hard_stage3.html"
     }
   ],
   easyKey: {
